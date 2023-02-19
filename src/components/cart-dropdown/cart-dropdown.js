30131559c142f601b5551688f9f78935d0af102a
ahmedGHANIM327 <ahmedghanim902@gmail.com>
2023-02-19 14:30:08 +0100

    Creating Checkout Page

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Button from '../button/button';
+import { CartContext } from '../../context/cart.context';
+
+import CartItem from '../cart-item/cart-item';
 
 import './cart-dropdown.scss';
 
 const CartDropdown = ({show,showCartAction}) => {
 
+    const navigate = useNavigate();
+
+    const navigateCheckout = () => {
+        navigate('/checkout');
+        showCartAction(false);
+    }
+
+    const {cartItems,cartTotal} = useContext(CartContext)
+
     const show_carte = show ? "cart-dropdown-container":"cart-dropdown-container hide_cart"
 
     return (<div className={show_carte}>
         <span className='close_carte' onClick={() => showCartAction(false)}>X</span>
-        <div className='cart-items' />
-        <Button>GO TO CHECKOUT</Button>
+        <div className='cart-items' >
+            {cartItems.length === 0 ? <h1>Your basket is empty</h1>:cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
+        </div>
+        <div className='cart-actions'>
+            <span className='cart-total'>Total : {cartTotal}£</span>
+            <Button onClick={navigateCheckout}>GO TO CHECKOUT</Button>
+        </div>
     </div>);
 };
 
--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -1,11 +1,17 @@
 import './cart-icon.scss'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag-icon.svg'
 
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
+
 const CartIcon = ({showCartAction}) => {
+
+    const {cartItemCount} = useContext(CartContext)
+
     return (
         <div className='cart-icon-container' onClick={() => showCartAction(true)}>
             <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartItemCount}</span>
         </div>
     )
 }
